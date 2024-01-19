247d6fc427b6270fd4d96ce63cbe56c63b060a83
Cedric Lapi <lapicedric@gmail.com>
2024-01-19 16:19:34 +0100

    backend completed

--- server/server.js
+++ server/server.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const cors = require('cors')
 const app = express();
-app.use(cors())
+
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 
 const port = 8000;
+require('./config/mongoose.config');
 require('./routes/person.routes')(app);
     
 app.listen(port, () => console.log(`Listening on port: ${port}`) );
