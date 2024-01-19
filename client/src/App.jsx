9e5fba698fa540a61ecf922cec1a1ce2b69fa551
Cedric Lapi <lapicedric@gmail.com>
2024-01-19 12:30:46 +0100

    Hello World!

---
+++ client/src/App.jsx
@@ -0,0 +1,15 @@
+
+
+import PersonForm from "./components/PersonForm";
+
+function App() {
+  
+
+  return (
+    <div className="App">
+      <PersonForm />
+    </div>
+  )
+}
+
+export default App
